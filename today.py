1ef19a9ec58dec2c50d1fd57ce0f38c8497bec1d
Fabio Mijango <105021697+FabioMijango@users.noreply.github.com>
2026-07-10 09:48:08 -0600

    Update today.py

--- today.py
+++ today.py
@@ -301,7 +301,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     justify_format(root, 'repo_data', repo_data, 8)
     justify_format(root, 'contrib_data', contrib_data)
     justify_format(root, 'follower_data', follower_data, 7)
-    justify_format(root, 'loc_data', loc_data[2], 9)
+    justify_format(root, 'loc_data', loc_data[2], 8)
     justify_format(root, 'loc_add', loc_data[0])
     justify_format(root, 'loc_del', loc_data[1], 2)
     tree.write(filename, encoding='utf-8', xml_declaration=True)
